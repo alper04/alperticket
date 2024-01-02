8881de3d9650f111d256b241d4a4b8b24fbecc9c
alper04 <128806070+alper04@users.noreply.github.com>
2024-01-02 18:00:02 +0300

    Update patch0.2.sql

--- patch0.2.sql
+++ patch0.2.sql
@@ -1,14 +1,16 @@
 CREATE TABLE transport_lines (
-	
-    line_id varchar(10) PRIMARY KEY,
-    line_city varchar(20) REFERENCES cities(city_name),
-	line_company varchar(30) REFERENCES companies(line_company)
-
+    line_id varchar(10),
+    line_city varchar(20),
+    line_company varchar(30),
+    PRIMARY KEY (line_id, line_city),
+    FOREIGN KEY (line_city) REFERENCES cities(city_name),
+    FOREIGN KEY (line_company) REFERENCES companies(line_company)
 );
 
 CREATE TABLE line_tickets (
-
-    line_id varchar(10) PRIMARY KEY REFERENCES transport_lines(line_id),
-    ticket_type int REFERENCES ticket_types(ticketType_id)
-
+    line_id varchar(10),
+    ticket_type int,
+    PRIMARY KEY (line_id, ticket_type),
+    FOREIGN KEY (line_id) REFERENCES transport_lines(line_id),
+    FOREIGN KEY (ticket_type) REFERENCES ticket_types(ticketType_id)
 );
